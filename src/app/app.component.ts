7f697fe75512db2bbd0c93fb08da7c09139f14d8
Gsantiago <sg955230@gmail.com>
2024-06-25 15:01:05 -0600

    version: se agrego......

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,38 +3,54 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'crud-empleados';
+  msg = '';
+  empleados = [
+    { name: 'Hugo', position: 'Programador Jr.', salary: 10000, age: 15, sex: 'M', email: 'hugo@disney.com' },
+    { name: 'Paco', position: 'Programador Sr.', salary: 15000, age: 18, sex: 'M', email: 'paco@disney.com' },
+    { name: 'Luis', position: 'Administrador Red', salary: 40000, age: 21, sex: 'M', email: 'luis@disney.com' }
+  ];
 
-  empleados=[
-    {name:'Hugo',position:'Programador Jr.',salary:10000,age:15,sex:'M'},
-    {name:'Paco',position:'Programador Sr.',salary:15000,age:18,sex:'M'},
-    {name:'Luis',position:'Administrador Red',salary:40000,age:21,sex:'M'}
-  ]
+  modelo = { name: '', position: '', salary: 0, age: 0, sex: '', email: '' };
+  modelo2 = { name: '', position: '', salary: 0, age: 0, sex: '', email: '' };
+  myValue = 0;
 
-  modelo:any={};
-
-  addEmpleado():void{
-    //Permite agregar un empleado al arreglo de empleados
-
-  } 
-  
-  deleteEmpleado():void{
-    //Permite eliminar un empleado del arreglo de empleados
+  addEmpleado(): void {
+    if (this.isValidEmpleado(this.modelo)) {
+      this.empleados.push({ ...this.modelo });
+      this.modelo = { name: '', position: '', salary: 0, age: 0, sex: '', email: '' };
+      this.msg = 'Empleado agregado!';
+    } else {
+      this.msg = 'Por favor, completa todos los campos.';
+    }
   }
 
-  editEmpleado():void{
-    //Permite seleccionar un empleado del arreglo de empleados para su actualización
-
+  deleteEmpleado(i: number): void {
+    this.empleados.splice(i, 1);
+    this.msg = 'Empleado eliminado!';
   }
 
-  updateEmpleado():void{
-    //Permite actualizar un empleado en el arreglo de empleados
-
+  editEmpleado(i: number): void {
+    this.myValue = i;
+    this.modelo2 = { ...this.empleados[i] };
+    this.msg = '';
   }
 
+  updateEmpleado(): void {
+    if (this.isValidEmpleado(this.modelo2)) {
+      let i = this.myValue;
+      this.empleados[i] = { ...this.modelo2 };
+      this.modelo2 = { name: '', position: '', salary: 0, age: 0, sex: '', email: '' };
+      this.msg = 'Empleado actualizado!';
+    } else {
+      this.msg = 'Por favor, completa todos los campos.';
+    }
+  }
 
-
+  isValidEmpleado(empleado: any): boolean {
+    return empleado.name && empleado.position && empleado.salary > 0 && empleado.age > 0 && empleado.sex && empleado.email;
+  }
 }
